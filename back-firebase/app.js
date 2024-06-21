7c52527dcd229e8ada7f1ee8c7c3e9a4a85e2d0b
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-06-21 17:37:42 +0800

    Firebase: init

---
+++ back-firebase/app.js
@@ -0,0 +1,19 @@
+const serviceAccount = require('./serviceAccountKey.json')
+
+const admin = require('firebase-admin')
+
+const functions = require('firebase-functions')
+
+const express = require('express')
+
+const app = express()
+
+const cors = require('cors')
+
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
+
+app.use(cors())
+
+app.get('/', (req, res) => res.json({ message: 'Success' }))
+
+exports.app = functions.https.onRequest(app)
